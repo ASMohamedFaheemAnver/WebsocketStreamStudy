031570f67212098fec9c7ef0f63e06296569ab69
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2024-04-01 23:27:02 +0530

    Convert camera and audio into data uri,

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,38 @@
+import { useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  const videoRef = useRef(null);
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        console.log({ stream });
+        // videoRef.current.srcObject = stream;
+        const mediaRecorder = new MediaRecorder(stream);
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          const data = e.target.result; // ~1.1MB if slice time is 2500ms
+          console.log({ data });
+        };
+
+        mediaRecorder.ondataavailable = async function (event) {
+          if (event.data && event.data.size > 0) {
+            reader.readAsDataURL(event.data);
+            // console.log({ data: URL.createObjectURL(event.data) });
+          }
+        };
+        mediaRecorder.start(2500); // slice time interval
+      })
+      .catch((e) => {
+        console.log({ component: App.name, e });
+      });
+  }, []);
+  return (
+    <div>
+      <video ref={videoRef} autoPlay />
+    </div>
+  );
 }
 
 export default App;
